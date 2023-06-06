4a2e5fd8500dc735f8b8a5cdc29ce5f99ccd8ccb
Artem <fallzdipper8@gmail.com>
2023-06-06 19:45:22 +0300

    finish authorizei

--- oop.py
+++ oop.py
@@ -1,7 +1,8 @@
-c = [10.5, False]
+c = [10.5]
 d = (5,'hello')
 t = {'hello' :5,
      10:'work'
 }
+
 
 print (t[10])
--- run.py
+++ run.py
@@ -10,17 +10,41 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = sa.Column(sa.Integer , primary_key = True)
     name = sa.Column(sa.Text)
+    password = sa.Column(sa.Text ,nullable = False)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/registraion',methods = ['GET','POST'])
 def index():
-    return 'hello' + request.method
+    name = request.form.get('name')
+    password = request.form.get('password')
+    if not name or not password:
+        return 'вы не ввели данные'
+    user = User(name = name, password = password)
+    db.session.add (user)
+    db.session.commit ()
+    return 'hello' + name
 
-@app.route('/auth')
+@app.route('/auth' ,methods = ['GET','POST'])
 def about():
-    return ('')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    if not name or not password:
+        return 'вы не ввели данные'
+    real_password = User.query.filter_by(name = name).first()
+    if not real_password or password != real_password.password:
+        return 'данные не совпадают'
+    return 'ok'
+
+#@app.route('/coincidence')
+#def aboutus():
+    
+
+    
+
+
+
 
 @app.errorhandler(404)
 def page_not_found(e):
